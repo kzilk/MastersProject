70de593b4c0529760e3525ccf1b1344da12ef3ac
quileso <oscardavidquiles@gmail.com>
2024-07-15 08:58:00 -0500

    Add files via upload

---
+++ ProjCode/Master's Project/ARPtable.py
@@ -0,0 +1,48 @@
+#import modules
+import re
+import os
+from collections import defaultdict
+
+#create ARP table to parse through its content
+def command():
+
+    os.system("arp -a > table.txt")
+
+    # puts the arp table into a temporary file
+    with open("table.txt", "r") as file:
+        table_contents = file.read()
+    return table_contents
+
+def search_table(table_contents):
+    expression = re.compile(r'\((.*?)\) at ([\w:]+)')
+    arp_results = defaultdict(list)
+
+    for pair in expression.findall(table_contents):
+        ip_add, mac_add = pair
+        arp_results[mac_add].append(ip_add)
+    return arp_results
+
+def repeat(arp_results):
+
+    # stores duplicate MAC addresses
+    doubles = {}
+
+    for mac_add, ip_add in arp_results.items():
+        if len(ip_add) > 1:
+            doubles[mac_add] = ip_add
+    return doubles
+
+
+def check_arp():
+    command_output = command()
+    arp_table = search_table(command_output)
+    arp_duplicate = repeat(arp_table)
+    if not arp_duplicate:
+            #print("YOU ARE SAFE. No duplicate MAC addresses located.")
+            return "GREEN"
+    else:
+            #print("WARNING! Your ARP tables contains duplicate MAC ADDRESSES!:")
+            return "RED"
+
+
+#print(check_arp())
---
+++ ProjCode/Master's Project/Network_Pass.py
@@ -0,0 +1,32 @@
+import os
+
+def testWindowsPassword():
+    # Get the connected network's SSID
+    os.system("netsh wlan show interfaces > network.txt")
+    wireless_name = None
+    with open("network.txt", "r") as file:
+        for line in file:
+            if "SSID" in line:
+                wireless_name = line.split(":")[1].strip()
+                break
+
+    # Get the profile details of the connected network
+    os.system(f"netsh wlan show profile name=\"{wireless_name}\" key=clear > pass_check.txt")
+    password = None
+    with open('pass_check.txt', 'r') as file:
+        for line in file:
+            if "Key Content" in line:
+                password = line.split(":")[1].strip()
+                break
+
+    # Check if a password was found
+    if password is not None:
+        #there is a password requirement
+        return "GREEN"
+    else:
+        #password field is null
+        return "RED"
+
+
+#test = testWindowsPassword()
+#print(test)
